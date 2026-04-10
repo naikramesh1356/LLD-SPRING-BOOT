4a616087f5274b0019fc2c47ccfce392688fc7ee
naikramesh1356 <naikramesh1356@gmail.com>
2026-04-11 01:26:20 +0530

    first commit

---
+++ src/main/java/com/example/newproject/model/Room.java
@@ -0,0 +1,9 @@
+package model;
+
+public class Room {
+    public int id;
+    public String name;
+    public String description;
+    public double price;
+    public String roomType;
+}
---
+++ src/main/java/com/example/newproject/model/User.java
@@ -0,0 +1,12 @@
+package model;
+
+public class User {
+    public int id;
+    public String role; // Admin or user
+
+    public User(int id, String role) {
+        this.id=id;
+        this.role=role;
+    }
+
+}
